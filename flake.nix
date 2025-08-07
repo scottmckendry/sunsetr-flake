5d988d40ce61ee084051136c05a4d6e6fb3f01b3
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-08-07 18:26:11 +1200

    feat: add flake.nix and flake.lock

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Flake for sunsetr";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      ver = "0.6.3";
+    in
+    {
+      packages.x86_64-linux.sunsetr = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
+        pname = "sunsetr";
+        version = "${ver}";
+        src = nixpkgs.legacyPackages.x86_64-linux.fetchurl {
+          url = "https://github.com/psi4j/sunsetr/releases/download/v${ver}/sunsetr-v${ver}-x86_64-linux.tar.gz";
+          sha256 = "sha256-+1vWYf3YTzy1YZIWDOYW4dwK0m+kxQvmhEIAP/mTiog=";
+        };
+        phases = [
+          "unpackPhase"
+          "installPhase"
+        ];
+        unpackPhase = ''
+          tar xvf $src
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp sunsetr-v${ver}/sunsetr $out/bin/
+        '';
+        meta = {
+          description = "Automatic blue light filter for Hyprland, Niri, and everything Wayland";
+          homepage = "https://github.com/psi4j/sunsetr";
+          license = nixpkgs.legacyPackages.x86_64-linux.lib.licenses.mit;
+          platforms = [ "x86_64-linux" ];
+        };
+      };
+      packages.x86_64-linux.default = self.packages.x86_64-linux.sunsetr;
+    };
+}
